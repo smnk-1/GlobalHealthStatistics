28e7d8df9b139c177354ddd0fe3ccb489abfc9cb
smnk-mcb <semikinnikolay1@gmail.com>
2024-12-09 13:52:20 +0600

    Data process

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ df = pd.read_csv('GlobalHealthStatistics.csv')
 print('First and last 5 lines')
 print(df.head(5))
 print(df.tail(5))
+
+print('\nData types')
+print(df.info())
+
+print('\nData static info:')
+print(df.describe())
+
+df_no_dup = df.drop_duplicates()
+print('\nКоличество строк до удаления дубликатов:', len(df))
+print('Количество строк после удаления дубликатов:', len(df_no_dup))
+
+df_processed = df_no_dup.rename(columns={'Average Treatment Cost (USD)': 'Average Treatment Cost ($$$)'})
