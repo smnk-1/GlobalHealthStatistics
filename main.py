5076e20fdbab8cdea93a854f5cb9dafd796c6aa8
smnk-1 <semikinnikolay1@gmail.com>
2024-12-22 19:36:08 +0600

    Tasks 5, 7, 8 completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import normaltest
 
 df = pd.read_csv('GlobalHealthStatistics.csv')
 
@@ -45,21 +46,49 @@ plt.title('Круговая диаграмма для Country')
 plt.savefig('pie_chart.png')
 plt.close()
 
-# # Тепловая карта корреляции
-# plt.figure(figsize=(10, 8))
-# corr_matrix = df_processed.corr()
-# sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f')
-# plt.title('Тепловая карта корреляции')
-# plt.savefig('heatmap.png')
-# plt.close()
+# One-hot кодирование всех категориальных признаков
+categorical_columns = ['Country', 'Disease Name', 'Disease Category',
+                       'Age Group', 'Gender', 'Treatment Type',
+                       'Availability of Vaccines/Treatment']
+df_encoded = pd.get_dummies(df_processed, columns=categorical_columns, drop_first=True)
+
+# Тепловая карта корреляции
+plt.figure(figsize=(50, 50))
+corr_matrix = df_encoded.corr()
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Тепловая карта корреляции')
+plt.savefig('heatmap.png')
+plt.close()
 
 # Диаграмма countplot с группировкой по двум номинативным признакам
-# plt.figure(figsize=(10, 8))
-# sns.countplot(data=df_processed, x='categorical_feature1', hue='categorical_feature2', palette='viridis')
-# plt.title('Диаграмма countplot для categorical_feature1 и categorical_feature2')
-# plt.xlabel('categorical_feature1')
-# plt.ylabel('Количество')
-# plt.savefig('countplot.png')
-# plt.close()
+plt.figure(figsize=(18, 16))
+sns.countplot(data=df_processed, x='Disease Category', hue='Gender', palette='viridis')
+plt.title('Диаграмма countplot для Disease Category и Gender')
+plt.xlabel('Disease Category')
+plt.ylabel('Количество')
+plt.savefig('countplot.png')
+plt.close()
+
+# Срез данных (первые 200 значений)
+subset = df_encoded['Prevalence Rate (%)'].dropna().head(200)  # Удаляем пропуски, если они есть
+
+# Проверка на нормальность
+stat, p_value = normaltest(subset)
+
+# Вывод результатов
+print(f"Statistics: {stat:.2f}, p-value: {p_value:.5f}")
+
+if p_value > 0.05:
+    print("Распределение нормальное (при уровне значимости 0.05)")
+else:
+    print("Распределение не является нормальным (при уровне значимости 0.05)")
+
+# Визуализация данных (гистограмма)
+plt.hist(subset, bins=20, alpha=0.7, color='blue', edgecolor='black')
+plt.title(f'Histogram of Prevalence Rate (%)')
+plt.xlabel('Prevalence Rate (%)')
+plt.ylabel("Frequency")
+plt.savefig('Histogram of Prevalence Rate (%)')
+
 
 print("Все графики построены и сохранены как PNG-файлы.")
